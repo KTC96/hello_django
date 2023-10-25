1fe40adcde1ad4985a5d249830851a00b1f75c8b
KTC96 <kyleclow96@gmail.com>
2023-10-25 20:14:15 +0000

    complete crud functionality

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
 
 
 def get_todo_list(request):
-    return render(request, "todo/todo_list.html")
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, "todo/todo_list.html", context)
+
+
+def add_item(request):
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('get_todo_list')
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, "todo/add_item.html", context)
+
+# If it's a get request then we'll just return the add_item HTML
+#  template by rendering it to them. But if it's a post request
+# we'll get the information from the form that comes in from this template.
+# And use it to create a new item.
+
+
+def edit_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+        return redirect('get_todo_list')
+    form = ItemForm(instance=item)
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/edit_item.html', context)
+
+
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    return redirect('get_todo_list')
+
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.delete()
+    return redirect('get_todo_list')
